7a23073e6a5a8b9a5d4732f9a2fd7f59708ffe6c
calmk666 <202803917@qq.com>
2026-03-31 15:51:52 +0800

    优化了whiletest.java文件的代码结构，添加了Scanner类的导入语句，并在main方法中添加了注释说明功能。

--- whiletest/whiletest.java
+++ whiletest/whiletest.java
@@ -2,20 +2,26 @@ package whiletest;
 
 import java.util.Scanner;
 
+
+
 public class whiletest {
-    public static void mian(String[] args){
-        //如果一个数是回文数,那么就打印True,如果不是回文数就返回false
-        Scanner sc = new Scanner(System.in);
-        System.out.println("请输入一个数:");
-        int x = sc.nextInt();
-        int term = x;
-        int num = 0;
-        while(x != 0){
-            int ge = x % 10;
-            x = x /10;
-            num = num * 10 + ge;
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+        try (//如果一个数是回文数,那么就打印True,如果不是回文数就返回false
+        Scanner sc = new Scanner(System.in)) {
+            System.out.println("请输入一个数:");
+            int x = sc.nextInt();
+            int term = x;
+            int num = 0;
+            while(x != 0){
+                int ge = x % 10;
+                x = x /10;
+                num = num * 10 + ge;
+            }
+            System.out.println(num == term);
         }
-        System.out.println(num == term);
     }
     
 }
